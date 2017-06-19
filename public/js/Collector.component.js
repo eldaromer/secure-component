85c1764d307f451a2d57c54fbd4a14e8d1fd47a2
Omer Eldar <eldaromer@gmail.com>
2017-06-19 00:27:13 -0600

    Added basic password validation, next step is to set up the  in a correct fashion in order to get correct validation for the form.

--- public/js/Collector.component.js
+++ public/js/Collector.component.js
@@ -12,10 +12,32 @@
         controller: function ($scope, $rootScope) {
 
             this.$onInit = function () {
-                console.log(this);
+                console.log($scope);
                 this.passUp({password: "test"});
-            }
+            };
+
+            $scope.validate = function () {
+                if (!/\d/g.test($scope.pass.password)) {
+                    this.passwordForm.password.$error.digits = true;
+                } else {
+                    this.passwordForm.password.$error.digits = false;
+                }
+                
+                if(!/[A-Za-z]/g.test($scope.pass.password)) {
+                    this.passwordForm.password.$error.alpha = true;
+                } else {
+                    this.passwordForm.password.$error.alpha = false;
+                }
+
+                if(($scope.pass.password + "").includes($scope.pass.username + "")) {
+                    this.passwordForm.password.$error.user = true;
+                    console.log(this);
+                } else {
+                    this.passwordForm.password.$error.user = false;
+                }
+
+            };
         }
     });
-    
+
 })();
